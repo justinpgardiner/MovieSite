e75ea310c6352772a7d1ac61d35864e6d85d9aaa
JansherA <jansh@gatech.edu>
2025-02-11 21:23:29 -0500

    Added Ch9

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Movie
+from .models import Movie, Review
 
 class MoveAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
 
 admin.site.register(Movie, MoveAdmin)
+admin.site.register(Review)
